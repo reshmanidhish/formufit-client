b6906b6723fe3babcbcc1892926b57478b0a1008
Didem Cakir <didemydn@gmail.com>
2023-09-01 14:32:07 +0200

    recipe images

--- src/pages/AllRecipesPage/index.jsx
+++ src/pages/AllRecipesPage/index.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Container } from "reactstrap";
+import Header from "components/Headers/Header";
+import { Card } from "reactstrap";
 
 function AllRecipes() {
   const [recipes, setRecipes] = useState([]);
@@ -10,7 +13,7 @@ function AllRecipes() {
 
     async function fetchRecipes() {
       try {
-        const storedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU5Y2MyMzg2ZWQ3ZjM2ZjM3NTE0ZTAiLCJlbWFpbCI6ImRpZGVtQHRlc3QuY29tIiwidXNlcm5hbWUiOiJkaWRvIiwidXQiOjEsImJvZHlUeXBlIjoxLCJpYXQiOjE2OTM1MTUyNzEsImV4cCI6MTY5MzUzNjg3MX0.XcvMZ5zYwGs8Yu2AUdHSwtVHznw6dXCJc3C_Ffbj42U"; 
+        const storedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU5Y2MyMzg2ZWQ3ZjM2ZjM3NTE0ZTAiLCJlbWFpbCI6ImRpZGVtQHRlc3QuY29tIiwidXNlcm5hbWUiOiJkaWRvIiwidXQiOjEsImJvZHlUeXBlIjoxLCJpYXQiOjE2OTM1Njk0MjAsImV4cCI6MTY5MzU5MTAyMH0.8nFczZ0CZvAupNNTq4ijMvkp95XsJGdIvMHzXZJ1xgY"
         const headers = {
         Authorization: `Bearer ${storedToken}`,
         };
@@ -27,6 +30,9 @@ function AllRecipes() {
   }, [])
   return (
     <> 
+    <Header breadcrumbName="Icon" breadcrumbIcon="fas fa-user" />
+    <Container fluid className="container-body">
+       
     {recipes.length ===0 ? 
     <p> No recipe found </p>:
   (<div>
@@ -34,15 +40,16 @@ function AllRecipes() {
     {recipes.map(recipe => {
       return <div key={recipe._id}>
         <Link to={`/recipes/${recipe._id}`}><h2>{recipe.title}</h2></Link>
-        <img src={recipe.image} alt="recipe_image"/>
-        <p>{recipe.ingredients}</p>
-        <p>{recipe.instructions}</p>
-        </div>
+        <img src={recipe.recipeImage} alt="recipe_image" className="img-thumbnail" style={{ width: "200px", height: "150px" }}/>
+       </div>
+       
     })}
   </div>
+  
     
   )}
-    
+  
+   </Container>  
 </>
   )}
   
