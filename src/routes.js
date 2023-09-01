a04594bf06fa2bd5cc9a25d2681c64327cfd4412
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-09-01 21:50:21 +0200

    guarding for user and admin roles

--- src/routes.js
+++ src/routes.js
@@ -16,50 +16,58 @@ const routes = [
     icon: "fas fa-home",
     component: <Index />,
     layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    ut: 0
   }, {
     path: "/workouts",
     name: "Workouts",
     icon: "fas fa-dumbbell",
     component: <AllWorkoutPage />,
     layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    ut: 0
   },{
     path: "/recipes/create",
     name: "Create recipes",
     icon: "fas fa-utensils",
     component: <CreateRecipes/>,
     layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    ut: 1
   },{
     path: "/recipes",
     name: "Diet recipes",
     icon: "fas fa-utensils",
     component: <Recipes/>,
     layout: "/main",
-    bodyTypeAvailable: true
-  },{
-    path: "/account",
-    name: "My Account 2",
-    icon: "fas fa-user",
-    component: <MyAccount />,
-    layout: "/main",
-    bodyTypeAvailable: true
-  },{
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: <Icons />,
-    layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    ut: 0
   },
+  // {
+  //   path: "/account",
+  //   name: "My Account 2",
+  //   icon: "fas fa-user",
+  //   component: <MyAccount />,
+  //   layout: "/main",
+  //   bodyTypeAvailable: true
+  // },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "ni ni-planet text-blue",
+  //   component: <Icons />,
+  //   layout: "/main",
+  //   bodyTypeAvailable: true
+  // },
   {
     path: "/user-profile",
     name: "User Profile",
     icon: "ni ni-single-02 text-yellow",
     component: <Profile />,
     layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    hidden: true,
+    ut: 0
   },
   {
     path: "/questionare",
@@ -67,23 +75,25 @@ const routes = [
     icon: "ni ni-single-02 text-yellow",
     component: <Questionare />,
     layout: "/main",
-    bodyTypeAvailable: false
+    bodyTypeAvailable: false,
+    ut: 0
   },
-  {
-    path: "/tables",
-    name: "Tables",
-    icon: "ni ni-bullet-list-67 text-red",
-    component: <Tables />,
-    layout: "/main",
-    bodyTypeAvailable: true
-  },
+  // {
+  //   path: "/tables",
+  //   name: "Tables",
+  //   icon: "ni ni-bullet-list-67 text-red",
+  //   component: <Tables />,
+  //   layout: "/main",
+  //   bodyTypeAvailable: true
+  // },
   {
     path: "/workout/create",
     name: "Create workout",
     icon: "ni ni-bullet-list-67 text-red",
     component: <WorkoutVideoUploadPage/>,
     layout: "/main",
-    bodyTypeAvailable: true
+    bodyTypeAvailable: true,
+    ut: 1
   }
 ];
 export default routes;
